1d22da8acc64603d06faad2a68ac7605eb717d43
Adam-Al-Rahman <quodscientialegis@gmail.com>
2024-04-04 05:39:38 +0530

    update: kernel density
    Kernel density algorithm implemented.
    Input: .csv
    output: .png, .tif
    Next Feature: to take input other shapefile and output .tif

--- homez/home_range/minimum_convex_polygon/minimum_convex_polygon.py
+++ homez/home_range/minimum_convex_polygon/minimum_convex_polygon.py
@@ -9,28 +9,27 @@ import matplotlib.pyplot as plt
 
 amphi = pd.read_csv("D:/Coding/HomeZ/homez/home_range/minimum_convex_polygon/tracking_sample.csv")
 
-
 amphi.dropna(subset=['x', 'y'], inplace=True)
 
-points = [Point(xy) for xy in zip(amphi['x'], amphi['y'])]# Converting into shapley points
+points = [Point(xy) for xy in zip(amphi['x'], amphi['y'])]  # Converting into shapley points
 
 # Calculating convex hull for different percentages
 mcp_results = []
 percentages = range(50, 105, 5)
 for percent in percentages:
-    num_points = int(len(points) * percent / 100)
-    if num_points < 3:
-        continue
-    mcp = MultiPoint(points[:num_points]).convex_hull
-    if not mcp.is_empty:  
-        mcp_results.append((percent, mcp))
+  num_points = int(len(points) * percent / 100)
+  if num_points < 3:
+    continue
+  mcp = MultiPoint(points[:num_points]).convex_hull
+  if not mcp.is_empty:
+    mcp_results.append((percent, mcp))
 
 # Plotting MCP
 fig, ax = plt.subplots(figsize=(10, 10))
 for percent, mcp in mcp_results:
-    if mcp.geom_type == 'Polygon':  
-        x, y = mcp.exterior.xy
-        ax.fill(x, y, alpha=0.5, label=f"{percent}% of points")
+  if mcp.geom_type == 'Polygon':
+    x, y = mcp.exterior.xy
+    ax.fill(x, y, alpha=0.5, label=f"{percent}% of points")
 ax.scatter(amphi['x'], amphi['y'], color='black', s=5, label='Points')
 plt.xlabel('X Coordinate')
 plt.ylabel('Y Coordinate')
